09c3196ca7cc79cbbee8bcbe7c8cd55df5b41fca
Thanatchon Chongmankhong <thanatchonchongmankhong@Thanatchons-MacBook-Air.local>
2021-11-22 13:59:59 +0700

    Update

--- javascripts/main.js
+++ javascripts/main.js
@@ -77,7 +77,7 @@ function loadData() {
     }
     else if (fileName.indexOf("PhraSiam") >= 0) {
         // categories = ["ORGANIZATION", "PERSON", "LOCATION", "LAW"];
-        categories = ["ORGANIZATION", "PERSON", "LOCATION", "LAW", "MONEY"];
+        categories = ["ORGANIZATION", "PERSON", "LOCATION", "LAW", "MONEY",'TIME'];
         loadAuthorData(draw, initTop);
     }
     else if (fileName.indexOf("VIS") >= 0) {
